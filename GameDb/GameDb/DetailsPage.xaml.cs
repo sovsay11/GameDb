86df142deefc9306da4fac6927d5d7992b8fb275
Noah <48299180+sovsay11@users.noreply.github.com>
2021-03-22 21:25:39 -0600

    Incorporated dynamic bars and labels for Pokemon stats!

--- GameDb/GameDb/DetailsPage.xaml.cs
+++ GameDb/GameDb/DetailsPage.xaml.cs
@@ -17,15 +17,11 @@ namespace GameDb
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailsPage : ContentPage
     {
-        // local variable for the pokemon name... might not even need this
-        public string pokeName { get; set; }
+        string pokeName, health, attack, defense, spAtk, spDef, spd;
+
         public DetailsPage(string name)
         {
             InitializeComponent();
-
-            pokeName = name;
-
-            //LblName.Text = pokeName;
 
             //List<string> names = new List<string>();
             using (WebClient wc = new WebClient())
@@ -44,25 +40,36 @@ namespace GameDb
 
                     ImgPokemon.Source = poke.sprites.front_default;
 
-                    LblName.Text = ConvertToUpper(poke.name);
-                    LblHP.Text = poke.stats[0].base_stat.ToString();
-                    LblAttack.Text = poke.stats[1].base_stat.ToString();
-                    LblDefense.Text = poke.stats[2].base_stat.ToString();
-                    LblSpecialAttack.Text = poke.stats[3].base_stat.ToString();
-                    LblSpecialDefense.Text = poke.stats[4].base_stat.ToString();
-                    LblSpeed.Text = poke.stats[5].base_stat.ToString();
+                    // assign to class variables
+                    pokeName = ConvertToUpper(poke.name);
+                    health = poke.stats[0].base_stat.ToString();
+                    attack = poke.stats[1].base_stat.ToString();
+                    defense = poke.stats[2].base_stat.ToString();
+                    spAtk = poke.stats[3].base_stat.ToString();
+                    spDef = poke.stats[4].base_stat.ToString();
+                    spd = poke.stats[5].base_stat.ToString();
+
+                    SetStatValues();
+
+                    AdjustBars();
+
+                    AdjustMargins();
 
                     if (poke.types.Count == 2)
                     {
                         LblType1.Text = ConvertToUpper(poke.types[0].type.name.ToString());
                         LblType2.Text = ConvertToUpper(poke.types[1].type.name.ToString());
+
+                        // need a method here to alter the color of the type to match
                     }
                     else
                     {
                         LblType1.Text = ConvertToUpper(poke.types[0].type.name.ToString());
                         Grid.SetColumnSpan(LblType1, 4);
+
+                        // need a method here to alter the color of the type to match
                     }
-                    
+
                 }
                 catch (Exception)
                 {
@@ -70,6 +77,52 @@ namespace GameDb
                 }
             }
         }
+
+        // adjusts stat bars
+        private void AdjustBars()
+        {
+            // ratio, max value of 200, calculate a percentage of that
+
+            BarHP.X2 = MaxStat(double.Parse(health));
+            BarAttack.X2 = MaxStat(double.Parse(attack));
+            BarDefense.X2 = MaxStat(double.Parse(defense));
+            BarSpecialAttack.X2 = MaxStat(double.Parse(spAtk));
+            BarSpecialDefense.X2 = MaxStat(double.Parse(spDef));
+            BarSpeed.X2 = MaxStat(double.Parse(spd));
+        }
+
+        private double MaxStat(double stat)
+        {
+            if (stat > 200)
+            {
+                stat = 200;
+            }
+            return stat;
+        }
+
+        // adjusts margins for stat values
+        private void AdjustMargins()
+        {
+            // ratio, max value of 120, calculate percentage of that
+            LblHP.Margin = new Thickness(MaxStat(double.Parse(health)) + 10, 0, 0, 0);
+            LblAttack.Margin = new Thickness(MaxStat(double.Parse(attack)) + 10, 0, 0, 0);
+            LblDefense.Margin = new Thickness(MaxStat(double.Parse(defense)) + 10, 0, 0, 0);
+            LblSpecialAttack.Margin = new Thickness(MaxStat(double.Parse(spAtk)) + 10, 0, 0, 0);
+            LblSpecialDefense.Margin = new Thickness(MaxStat(double.Parse(spDef)) + 10, 0, 0, 0);
+            LblSpeed.Margin = new Thickness(MaxStat(double.Parse(spd)) + 10, 0, 0, 0);
+        }
+
+        private void SetStatValues()
+        {
+            LblName.Text = pokeName;
+            LblHP.Text = health;
+            LblAttack.Text = attack;
+            LblDefense.Text = defense;
+            LblSpecialAttack.Text = spAtk;
+            LblSpecialDefense.Text = spDef;
+            LblSpeed.Text = spd;
+        }
+
         private string ConvertToUpper(string text)
         {
             text = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text);
