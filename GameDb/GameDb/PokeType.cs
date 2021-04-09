c50a4105eea374830bfa69943c1aa06c44fb03e3
Noah <48299180+sovsay11@users.noreply.github.com>
2021-04-08 18:50:53 -0600

    Finished primary data for the PokeType class.

--- GameDb/GameDb/PokeType.cs
+++ GameDb/GameDb/PokeType.cs
@@ -101,24 +101,32 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
                 { "Ground", 2 },
+                { "Rock", 2 },
                 { "Water", 2 },
-                { "Grass", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
+                { "Grass", 2 },
+                { "Ice", 2 },
+                { "Bug", 2 },
+                { "Steel", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Ground", 0.5 },
-                { "Rock", 0.5 },
+                { "Fire", 0.5 },
                 { "Water", 0.5 },
+                { "Rock", 0.5 },
+                { "Dragon", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Water", 0.5 },
+                { "Fire", 0.5 },
+                { "Grass", 0.5 },
+                { "Ice", 0.5 },
+                { "Bug", 0.5 },
+                { "Steel", 0.5 },
+                { "Fairy", 0.5 },
             };
         }
     }
@@ -132,31 +140,27 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
-                { "Water", 2 },
+                { "Electric", 2 },
                 { "Grass", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
+                { "Fire", 2 },
                 { "Ground", 2 },
-                { "Water", 2 },
-                { "Grass", 2 },
+                { "Rock", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
-                { "Water", 2 },
-                { "Grass", 2 },
+                { "Water", 0.5 },
+                { "Grass", 0.5 },
+                { "Dragon", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
-                { "Water", 2 },
-                { "Grass", 2 },
+                { "Fire", 0.5 },
+                { "Water", 0.5 },
+                { "Ice", 0.5 },
+                { "Steel", 0.5 },
             };
         }
     }
@@ -170,23 +174,25 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
                 { "Ground", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Water", 2 },
+                { "Flying", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Electric", 0.5 },
+                { "Grass", 0.5 },
+                { "Ground", 0 },
+                { "Dragon", 0 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Electric", 0.5 },
+                { "Flying", 0.5 },
+                { "Steel", 0.5 },
             };
         }
     }
@@ -200,23 +206,34 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fire", 2 },
+                { "Ice", 2 },
+                { "Poison", 2 },
+                { "Flying", 2 },
+                { "Bug", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
+                { "Water", 2 },
                 { "Ground", 2 },
+                { "Rock", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fire", 0.5 },
+                { "Grass", 0.5 },
+                { "Poison", 0.5 },
+                { "Flying", 0.5 },
+                { "Bug", 0.5 },
+                { "Dragon", 0.5 },
+                { "Steel", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Water", 0.5 },
+                { "Electric", 0.5 },
+                { "Grass", 0.5 },
+                { "Ground", 0.5 },
             };
         }
     }
@@ -230,23 +247,28 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
+                { "Fire", 2 },
+                { "Fighting", 2 },
                 { "Rock", 2 },
-                { "Ground", 2 },
+                { "Steel", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
+                { "Grass", 2 },
                 { "Ground", 2 },
+                { "Flying", 2 },
+                { "Dragon", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fire", 0.5 },
+                { "Water", 0.5 },
+                { "Ice", 0.5 },
+                { "Steel", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Ice", 0.5 },
             };
         }
     }
@@ -260,23 +282,32 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Flying", 2 },
+                { "Psychic", 2 },
+                { "Fairy", 2 },
             };
             strengths = new Dictionary<string, double>
             {
+                { "Normal", 2 },
+                { "Ice", 2 },
                 { "Rock", 2 },
-                { "Ground", 2 },
+                { "Dark", 2 },
+                { "Steel", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Poison", 0.5 },
+                { "Flying", 0.5 },
+                { "Psychic", 0.5 },
+                { "Bug", 0.5 },
+                { "Ghost", 0 },
+                { "Fairy", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Bug", 0.5 },
+                { "Rock", 0.5 },
+                { "Dark", 0.5 },
             };
         }
     }
@@ -289,23 +320,29 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
                 { "Ground", 2 },
+                { "Psychic", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Grass", 2 },
+                { "Fairy", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Poison", 0.5 },
+                { "Ground", 0.5 },
+                { "Rock", 0.5 },
+                { "Ghost", 0.5 },
+                { "Steel", 0 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Grass", 0.5 },
+                { "Fighting", 0.5 },
+                { "Poison", 0.5 },
+                { "Bug", 0.5 },
+                { "Fairy", 0.5 },
             };
         }
     }
@@ -318,23 +355,29 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Water", 2 },
+                { "Grass", 2 },
+                { "Ice", 2 },
             };
             strengths = new Dictionary<string, double>
             {
+                { "Fire", 2 },
+                { "Electric", 2 },
+                { "Poison", 2 },
                 { "Rock", 2 },
-                { "Ground", 2 },
+                { "Steel", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Grass", 0.5 },
+                { "Flying", 0 },
+                { "Bug", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Electric", 0 },
+                { "Poison", 0.5 },
+                { "Rock", 0.5 },
             };
         }
     }
@@ -347,23 +390,28 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
+                { "Electric", 2 },
+                { "Ice", 2 },
                 { "Rock", 2 },
-                { "Ground", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Grass", 2 },
+                { "Fighting", 2 },
+                { "Bug", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Electric", 0.5 },
+                { "Rock", 0.5 },
+                { "Steel", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Grass", 0.5 },
+                { "Fighting", 0.5 },
+                { "Ground", 0 },
+                { "Bug", 0.5 },
             };
         }
     }
@@ -376,23 +424,25 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Bug", 2 },
+                { "Ghost", 2 },
+                { "Dark", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fighting", 2 },
+                { "Poison", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Psychic", 0.5 },
+                { "Dark", 0 },
+                { "Steel", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fighting", 0.5 },
+                { "Psychic", 0.5 },
             };
         }
     }
@@ -405,23 +455,31 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
+                { "Fire", 2 },
+                { "Flying", 2 },
                 { "Rock", 2 },
-                { "Ground", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Grass", 2 },
+                { "Psychic", 2 },
+                { "Dark", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fire", 0.5 },
+                { "Fighting", 0.5 },
+                { "Poison", 0.5 },
+                { "Flying", 0.5 },
+                { "Ghost", 0.5 },
+                { "Steel", 0.5 },
+                { "Fairy", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Grass", 0.5 },
+                { "Fighting", 0.5 },
+                { "Ground", 0.5 },
             };
         }
     }
@@ -434,23 +492,31 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
+                { "Water", 2 },
+                { "Grass", 2 },
+                { "Fighting", 2 },
                 { "Ground", 2 },
+                { "Steel", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fire", 2 },
+                { "Ice", 2 },
+                { "Flying", 2 },
+                { "Bug", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fighting", 0.5 },
+                { "Ground", 0.5 },
+                { "Steel", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Normal", 0.5 },
+                { "Fire", 0.5 },
+                { "Poison", 0.5 },
+                { "Flying", 0.5 },
             };
         }
     }
@@ -464,23 +530,25 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Ghost", 2 },
+                { "Dark", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Psychic", 2 },
+                { "Ghost", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Normal", 0 },
+                { "Dark", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Normal", 0 },
+                { "Fighting", 0 },
+                { "Poison", 0.5 },
+                { "Bug", 0.5 },
             };
         }
     }
@@ -493,23 +561,25 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Ice", 2 },
+                { "Dragon", 2 },
+                { "Fairy", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Dragon", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Steel", 0.5 },
+                { "Fairy", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fire", 0.5 },
+                { "Water", 0.5 },
+                { "Electric", 0.5 },
+                { "Grass", 0.5 },
             };
         }
     }
@@ -522,23 +592,26 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fighting", 2 },
+                { "Bug", 2 },
+                { "Fairy", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Psychic", 2 },
+                { "Ghost", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fighting", 0.5 },
+                { "Dark", 0.5 },
+                { "Fairy", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Psychic", 0 },
+                { "Ghost", 0.5 },
+                { "Dark", 0.5 },
             };
         }
     }
@@ -551,23 +624,36 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
+                { "Fire", 2 },
+                { "Fighting", 2 },
                 { "Ground", 2 },
             };
             strengths = new Dictionary<string, double>
             {
+                { "Ice", 2 },
                 { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fairy", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fire", 0.5 },
+                { "Water", 0.5 },
+                { "Electric", 0.5 },
+                { "Steel", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Normal", 0.5 },
+                { "Grass", 0.5 },
+                { "Ice", 0.5 },
+                { "Poison", 0 },
+                { "Flying", 0.5 },
+                { "Psychic", 0.5 },
+                { "Bug", 0.5 },
+                { "Rock", 0.5 },
+                { "Dragon", 0.5 },
+                { "Steel", 0.5 },
+                { "Fairy", 0.5 },
             };
         }
     }
@@ -580,23 +666,27 @@ namespace GameDb
 
             vulnerabilities = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Poison", 2 },
+                { "Steel", 2 },
             };
             strengths = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fighting", 2 },
+                { "Dragon", 2 },
+                { "Dark", 2 },
             };
             weaknesses = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fire", 0.5 },
+                { "Poison", 0.5 },
+                { "Steel", 0.5 },
             };
             resistances = new Dictionary<string, double>
             {
-                { "Rock", 2 },
-                { "Ground", 2 },
+                { "Fighting", 0.5 },
+                { "Bug", 0.5 },
+                { "Dragon", 0 },
+                { "Dark", 0.5 },
             };
         }
     }
